b20f624e46173ab345a291091c03927148a38d63
pm.deblois <pm.deblois@048f5755-6c8a-a6fa-e8cf-882046aa2633>
2012-03-27 04:34:24 +0000

    + map creation from map.txt

--- coal-raider/coal-raider/coal-raider/Gameplay/Map.cs
+++ coal-raider/coal-raider/coal-raider/Gameplay/Map.cs
@@ -11,29 +11,45 @@ namespace coal_raider
 {
     class Map : Microsoft.Xna.Framework.GameComponent
     {
-
-        public Model model { get; protected set; }
+        private Game game;
+        private Model model;
         protected Matrix world = Matrix.Identity;
-        public float leftXPos { get; protected set; }
-        public float bottomYPos { get; protected set; }
+        public List<StaticObject> staticObjects { get; protected set; }
 
+        /*
         RasterizerState originalState, transparentState;
-
         DepthStencilState first, second, original;
+        */
 
         GraphicsDevice graphics;
 
-        public Map(Game game, Model[] modelComponents, float leftX, float bottomY, GraphicsDevice graphics)
+        string mapFileName = ".\\map.txt";
+
+        public Map(Game game, Model[] modelComponents, GraphicsDevice graphics)
             : base(game)
         {
-            // Set model attribute
-            model = modelComponents[0];
-
-            leftXPos = leftX;
-            bottomYPos = bottomY;
-
+            this.game = game;
             this.graphics = graphics;
 
+            // Set models
+            model = modelComponents[0];
+
+            Model[] building = new Model[1];
+            building[0] = modelComponents[1];
+
+            Model[] tree = new Model[1];
+            tree[0] = modelComponents[2];
+
+            Model[][] modelArrays = {
+                                        building,
+                                        tree
+                                    };
+
+            staticObjects = new List<StaticObject>();
+
+            buildMapFromFile(mapFileName, modelArrays);
+
+            /*
             originalState = new RasterizerState();
             originalState.CullMode = CullMode.CullCounterClockwiseFace;
 
@@ -49,6 +65,59 @@ namespace coal_raider
             second.DepthBufferWriteEnable = false;
 
             original = graphics.DepthStencilState;
+            */
+        }
+
+        private void buildMapFromFile(string fileName, Model[][] modelArrays)
+        {
+            StreamReader sr = new StreamReader(fileName);
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (sr)
+                {
+                    char[] delimiterChars = { ' ', ',', '\n', ')', '(', '/' };
+                    string[] words = new string[10];
+                    String line;
+                    int x = 0,
+                        y = 0;
+
+                    Vector2 mapSize = new Vector2();
+                    if ((line = sr.ReadLine()) != null)
+                    {
+                        words = line.Split(delimiterChars);
+                        mapSize = new Vector2(int.Parse(words[0]), int.Parse(words[1]));
+                    }
+
+
+                    // Read and display lines from the file until the end of
+                    // the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        x = 0;
+                        ++y;
+
+                        words = line.Split(delimiterChars);
+                        foreach (string i in words)
+                        {
+                            if (i.Equals("-"))
+                            {
+                                ++x;
+                                continue;
+                            }
+                            staticObjects.Add(new StaticObject(game, modelArrays[int.Parse(i)], new Vector3((x++ * 2) - mapSize.X, 0, (2 * y) - mapSize.Y)));
+                        }
+                        
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong.
+                Console.WriteLine("The map file could not be read:");
+                Console.WriteLine(e.Message);
+            }
         }
 
         public void Draw(Camera camera)
@@ -70,6 +139,10 @@ namespace coal_raider
                 mesh.Draw();
             }
 
+            foreach (StaticObject so in staticObjects)
+            {
+                so.Draw(camera);
+            }
         }
     }
 }
---
+++ coal-raider/coal-raider/coal-raider/Gameplay/StaticObject.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace coal_raider
+{
+    class StaticObject : Object
+    {
+        public Model model { get; protected set; }
+
+        public StaticObject(Game game, Model[] modelComponents, Vector3 position)
+            : base(game, modelComponents, position, true)
+        {
+            model = modelComponents[0];
+            world = /*Matrix.CreateRotationY(MathHelper.ToRadians(angle)) * */ Matrix.CreateTranslation(position);
+            this.position = position;
+        }
+
+        public override void Draw(Camera camera)
+        {
+            Matrix[] transforms = new Matrix[model.Bones.Count];
+            model.CopyAbsoluteBoneTransformsTo(transforms);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (BasicEffect be in mesh.Effects)
+                {
+                    be.EnableDefaultLighting();
+                    be.SpecularPower = 10f;
+                    be.Projection = camera.projection;
+                    be.View = camera.view;
+                    be.World = world * mesh.ParentBone.Transform;
+                }
+                mesh.Draw();
+            }
+        }
+    }
+}
