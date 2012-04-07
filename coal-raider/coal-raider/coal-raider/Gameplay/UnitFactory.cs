d54836a66e3650dc02bdeaedd501765884c0829e
pm.deblois <pm.deblois@048f5755-6c8a-a6fa-e8cf-882046aa2633>
2012-04-07 20:14:59 +0000

    + added intersection with boundingFrustrum for squads + fixed specular light for static objects + Created update animation function + improvements to UnitFactory.cs + added selection of squads and drawing of a model when selected

--- coal-raider/coal-raider/coal-raider/Gameplay/UnitFactory.cs
+++ coal-raider/coal-raider/coal-raider/Gameplay/UnitFactory.cs
@@ -23,41 +23,62 @@ namespace coal_raider
             switch(type)
             {
                 case UnitType.Mage:
-                    return createMage(game, modelComponents, position, -80f, 15f, 10f, 25f, 2000f);
+                    return createMage(game, modelComponents, position);
                 case UnitType.Ranger:
-                    return createRanger(game, modelComponents, position, -30f, 0f, 10f, 40f, 1000f);
+                    return createRanger(game, modelComponents, position);
                 case UnitType.Warrior:
-                    return createWarrior(game, modelComponents, position, -80f, 15f, 10f, 1.5f, 300f);
+                    return createWarrior(game, modelComponents, position);
             }
             return null;
         }
 
-        private static Unit createMage(Game game, Model[] modelComponents, Vector3 position,
-                                       float armUpAngle, float armDownAngle, float armRotationSpeed, float attackRange, float attackRate)
+        private static Unit createMage(Game game, Model[] modelComponents, Vector3 position)
         {
             int topHP = 100;
             int topSP = 100;
             float speed = 0.05f;
+
+            float attackRange = 25f;
+            float attackRate = 2000f; //in miliseconds
+
+            float armUpAngle = -80f;
+            float armDownAngle = 15f;
+            float armRotationSpeed = 10f;
+            
             return new Unit(game, modelComponents, position, UnitType.Mage, topHP, topSP, speed, true, 
                             armUpAngle, armDownAngle, armRotationSpeed, attackRange, attackRate);
         }
 
-        private static Unit createRanger(Game game, Model[] modelComponents, Vector3 position,
-                                         float armUpAngle, float armDownAngle, float armRotationSpeed, float attackRange, float attackRate)
+        private static Unit createRanger(Game game, Model[] modelComponents, Vector3 position)
         {
             int topHP = 100;
             int topSP = 50;
             float speed = 0.07f;
+
+            float attackRange = 40f;
+            float attackRate = 1000f; //in miliseconds
+
+            float armUpAngle = -30f;
+            float armDownAngle = 0f;
+            float armRotationSpeed = 10f;
+
             return new Unit(game, modelComponents, position, UnitType.Ranger, topHP, topSP, speed, true,
                             armUpAngle, armDownAngle, armRotationSpeed, attackRange, attackRate);
         }
 
-        private static Unit createWarrior(Game game, Model[] modelComponents, Vector3 position, 
-                                          float armUpAngle, float armDownAngle, float armRotationSpeed, float attackRange, float attackRate)
+        private static Unit createWarrior(Game game, Model[] modelComponents, Vector3 position)
         {
             int topHP = 200;
             int topSP = 20;
             float speed = 0.06f;
+
+            float attackRange = 1.5f;
+            float attackRate = 300f; //interval in miliseconds
+
+            float armUpAngle = -80f;
+            float armDownAngle = 15f;
+            float armRotationSpeed = 10f;
+
             return new Unit(game, modelComponents, position, UnitType.Warrior, topHP, topSP, speed, true,
                             armUpAngle, armDownAngle, armRotationSpeed, attackRange, attackRate);
         }
