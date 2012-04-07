c57d257abf2fe57bd685203a93fa78d69a47d418
ncorsale <ncorsale@048f5755-6c8a-a6fa-e8cf-882046aa2633>
2012-04-07 23:14:32 +0000

    - Added a new squad formation: Flank - Added functionality that when a squad finishes an attack (go from attacking to not attacking), it will check the squad integrity. It will find the best suited squad formation, fill it and set necessary squad values...At least I'm pretty sure it will. I cannot test it as we cannot attack or die yet.

--- trunk/coal-raider/coal-raider/coal-raider/Gameplay/Squad.cs
+++ trunk/coal-raider/coal-raider/coal-raider/Gameplay/Squad.cs
@@ -11,7 +11,7 @@ namespace coal_raider
 {
     class Squad : NavigatingObject
     {
-        private int numUnitsInFormation;
+        public int numUnitsInFormation;
 
         public Object target { get; protected set; }
         public float avgSpeed;
@@ -25,6 +25,9 @@ namespace coal_raider
         // Moved this out of Update function
         Random rand = new Random();
 
+        public bool wasAttacking = false, verifySquad = false;
+        int unitsBeforeAttack;
+
         public Squad(Game game, Unit[] unitList, int numUnitsInFormation, Vector3[] formationOffset, SquadSlotType[] formationSlotTypes)
             : base(game, null, new Vector3(0,0,0), true, false)
         {
@@ -34,6 +37,7 @@ namespace coal_raider
             this.formationOffset = formationOffset;
             this.formationSlotTypes = formationSlotTypes;
             this.numUnitsInFormation = numUnitsInFormation;
+            this.unitsBeforeAttack = unitList.Length;
             this.unitList = placeUnits(unitList);
 
             bigestRange = float.MinValue;
@@ -102,6 +106,24 @@ namespace coal_raider
             return Enumerable.Reverse(d.Values).ToArray();
         }
 
+        private float totalAssignmentEase(Unit[] uList, SquadSlotType[] sst)
+        {
+            float total = 0;
+
+            foreach (Unit u in uList)
+            {
+                float ease = 0;
+                for (int i = 0; i < sst.Length; ++i)
+                {
+                    ease += 1 / (1 + SquadFactory.getSlotCost(u.type, sst[i]));
+                }
+
+                total += ease;
+            }
+
+            return total;
+        }
+
         public override void Update(GameTime gameTime, SpatialHashGrid grid, List<Waypoint> waypointList)
         {
             updatePosition();
@@ -127,11 +149,17 @@ namespace coal_raider
                 {
                     // If so, set the unit target to the squad target
                     unitList[i].setTarget(target.position, velocity);
+                    wasAttacking = true;
                     //unitList[i].attacking = true;
                 }
                 // Otherwise
                 else
                 {
+                    // If we were attacking and are no longer, check if squad formation needs
+                    // to be changed
+                    if (wasAttacking)
+                        checkSquad();
+
                     // Set the unit target to the squad position
                     unitList[i].setTarget(newPos, velocity);
                     //unitList[i].attacking = false;
@@ -191,6 +219,148 @@ namespace coal_raider
                 }
             }
             return false;
+        }
+
+        private void checkSquad()
+        {
+            int currentUnitCount = unitList.Length;
+            // If we lost units, get a new formation
+            if (unitsBeforeAttack != currentUnitCount)
+            {
+                // Set default values for squad type
+                SquadType bestType = SquadType.Square;
+
+                #region Get Best Formation
+
+                float minCost = 999999;
+                // Find most suitable formation by current unit count (dont choose formation with less units
+                // then we currently have)
+                if (currentUnitCount < SquadFactory.getFormationUnitCount(SquadType.Pentagram))
+                {
+                    // Get slot type information
+                    SquadSlotType[] pSlots = SquadFactory.getPentagramFormationSlotTypes();
+
+                    float cost = 0;
+                    // For each unit in the current unit list
+                    foreach (Unit u in unitList)
+                    {
+                        // Get the total assignment ease from using this formation
+                        cost = totalAssignmentEase(unitList, pSlots);
+                    }
+
+                    if (cost < minCost)
+                    {
+                        minCost = cost;
+                        bestType = SquadType.Pentagram;
+                    }
+                }
+
+                if (currentUnitCount < SquadFactory.getFormationUnitCount(SquadType.Flank))
+                {
+                    // Get slot type information
+                    SquadSlotType[] pSlots = SquadFactory.getFlankFormationSlotTypes();
+
+                    float cost = 0;
+                    // For each unit in the current unit list
+                    foreach (Unit u in unitList)
+                    {
+                        // Get the total assignment ease from using this formation
+                        cost = totalAssignmentEase(unitList, pSlots);
+                    }
+
+                    if (cost < minCost)
+                    {
+                        minCost = cost;
+                        bestType = SquadType.Flank;
+                    }
+                }
+
+                if (currentUnitCount < SquadFactory.getFormationUnitCount(SquadType.Pyramid))
+                {
+                    // Get slot type information
+                    SquadSlotType[] pSlots = SquadFactory.getPyramidFormationSlotTypes();
+
+                    float cost = 0;
+                    // For each unit in the current unit list
+                    foreach (Unit u in unitList)
+                    {
+                        // Get the total assignment ease from using this formation
+                        cost = totalAssignmentEase(unitList, pSlots);
+                    }
+
+                    if (cost < minCost)
+                    {
+                        minCost = cost;
+                        bestType = SquadType.Pyramid;
+                    }
+                }
+
+                if (currentUnitCount < SquadFactory.getFormationUnitCount(SquadType.Square))
+                {
+                    // Get slot type information
+                    SquadSlotType[] pSlots = SquadFactory.getSquareFormationSlotTypes();
+
+                    float cost = 0;
+                    // For each unit in the current unit list
+                    foreach (Unit u in unitList)
+                    {
+                        // Get the total assignment ease from using this formation
+                        cost = totalAssignmentEase(unitList, pSlots);
+                    }
+
+                    if (cost < minCost)
+                    {
+                        minCost = cost;
+                        bestType = SquadType.Square;
+                    }
+                }
+                #endregion
+
+                // Get the new formation information and activate new formation
+                switch(bestType)
+                {
+                    case SquadType.Flank:
+                        this.formationOffset = SquadFactory.getFlankFormationOffset();
+                        this.formationSlotTypes = SquadFactory.getFlankFormationSlotTypes();
+                        this.unitList = placeUnits(unitList);
+                        break;
+
+                    case SquadType.Pentagram:
+                        this.formationOffset = SquadFactory.getPentagramFormationOffset();
+                        this.formationSlotTypes = SquadFactory.getPentagramFormationSlotTypes();
+                        this.unitList = placeUnits(unitList);
+                        break;
+
+                    case SquadType.Pyramid:
+                        this.formationOffset = SquadFactory.getPyramidFormationOffset();
+                        this.formationSlotTypes = SquadFactory.getPyramidFormationSlotTypes();
+                        this.unitList = placeUnits(unitList);
+                        break;
+
+                    case SquadType.Square:
+                        this.formationOffset = SquadFactory.getSquareFormationOffset();
+                        this.formationSlotTypes = SquadFactory.getSquareFormationSlotTypes();
+                        this.unitList = placeUnits(unitList);
+                        break;
+
+                    default:
+                        // This is an error state that should never be reached
+                        // In any case, leave squad formation as is
+                        break;
+                }
+
+                // Set other necessary information for formation operation
+                bigestRange = float.MinValue;
+                float totalSpeed = 0;
+                foreach (Unit u in unitList)
+                {
+                    totalSpeed += u.speed;
+                    if (u.attackRange > bigestRange) bigestRange = u.attackRange;
+                }
+                avgSpeed = totalSpeed / numUnitsInFormation;
+
+                target = null;
+            }
         }
     }
 }
