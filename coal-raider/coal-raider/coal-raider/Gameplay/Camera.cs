f45da2a9a432d422c3fd9cafa6c6671da9df3fba
yipksj <yipksj@048f5755-6c8a-a6fa-e8cf-882046aa2633>
2012-04-11 13:59:15 +0000

    better camera limits, new tutorial image

--- coal-raider/coal-raider/coal-raider/Gameplay/Camera.cs
+++ coal-raider/coal-raider/coal-raider/Gameplay/Camera.cs
@@ -54,7 +54,7 @@ namespace coal_raider
             noActionRectangle = new Rectangle(MouseCameraBorder, MouseCameraBorder, vp.Width - MouseCameraBorder * 2, vp.Height - MouseCameraBorder * 2);
             screenSizeOver2 = new Vector2(vp.Width / 2, vp.Height / 2);
 
-            this.mapSize = mapSize - new Vector2(5, 5);
+            this.mapSize = mapSize - new Vector2(20, 20);
         }
 
         public override void Initialize()
@@ -80,11 +80,11 @@ namespace coal_raider
                     cameraTarget.X -= msV.X * mouseMouvementScalar;
                 }
 
-                if ((cameraTarget.Z >= -mapSize.Y && cameraTarget.Z <= mapSize.Y))
+                if ((cameraTarget.Z >= -mapSize.Y && cameraTarget.Z <= mapSize.Y-7))
                 {
                     cameraTarget.Z += msV.Z * mouseMouvementScalar;
                 }
-                if (!(cameraTarget.Z >= -mapSize.Y && cameraTarget.Z <= mapSize.Y))//undo move if out of screen
+                if (!(cameraTarget.Z >= -mapSize.Y && cameraTarget.Z <= mapSize.Y-7))//undo move if out of screen
                 {
                     cameraTarget.Z -= msV.Z * mouseMouvementScalar;
                 }
